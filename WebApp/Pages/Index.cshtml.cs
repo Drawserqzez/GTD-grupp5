958bb8afed2cabfbe28ac69774f92b643b76d34d
Joakim Kvist <joakim.kvist@outlook.com>
2019-11-28 09:07:37 +0100

    Added tablerows to index. Commit to get last props

--- WebApp/Pages/Index.cshtml.cs
+++ WebApp/Pages/Index.cshtml.cs
@@ -10,26 +10,26 @@ using Classlibrary;
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
-    {
-       public List<string> TestToDo = new List<string>();
-       public List<string> TestDoing = new List<string>();
-       public List<string> TestDone = new List<string>();
+    {       
+        public List<Classlibrary.Task> taskList;
+        public Classlibrary.Task task;
+        public bool sortAt {get; set;}
 
-       
-       public List<Classlibrary.Task> taskList;
-       public Classlibrary.Task task;
-        public void OnGet()
+        public void OnGet(int SortNum)
         {
-           taskList = Startup.todoHandler.GetTasks();
-        //    task = taskList[0];
-        //    task. 
+            if (SortNum == 1)
+                {sortAt = true;}
+            else if (SortNum == 0)
+                {sortAt = false;}
+            taskList = Startup.todoHandler.GetTasks(sortAt);
         }
         
 
         public void doingMove() 
         {
-            
-        }
+           // string majs = Classlibrary.TodoHandler._taskList;
+           // string majs = Classlibrary.Task.
+                 }
 
        
     }
